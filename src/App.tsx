74b63822e956d75923cb9e03e8bd3205a6e9a3d6
öthiagofelippe <thiagofelippe135@gmail.com>
2025-09-16 11:40:00 -0300

    feat: add CountDown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from "./components/Container";
+import { CountDown } from "./components/CountDown";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
@@ -11,6 +12,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
