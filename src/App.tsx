6c1cbca33000e362675bd34a2dfbf7eb287953f6
öthiagofelippe <thiagofelippe135@gmail.com>
2025-09-15 23:26:10 -0300

    feat: add Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Timer } from "lucide-react";
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>
-          Olá mundo
-          <Timer />
-        </Heading>
+        <Logo/>
       </Container>
 
       <Container>
